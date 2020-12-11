025677b12cf3bb8c887fa9564bc91a5579255f6d
chayadeaver <chayadeaver@gmail.com>
2020-12-11 14:32:17 -0500

    added event listener

---
+++ app.js
@@ -0,0 +1,19 @@
+// Book Constructor
+function Book (title, author, isbn){
+    this.title = title
+    this.author = author
+    this.isbn = isbn
+}
+
+// UI Constructor
+function UI(){}
+
+// Event listeners
+document.getElementById('book-form').addEventListener('submit', function(e){
+    const title = document.getElementById('title').value,
+          author = document.getElementById('author').value,
+          isbn = document.getElementById('isbn').value
+
+    console.log(title, author, isbn)
+    e.preventDefault()
+})
